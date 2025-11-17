d8c5f60d09e04d0d7ba0ef0bab74f35e525f4b9f
Jerboas86 <benoitdelemps@gmail.com>
2025-11-17 23:10:44 +0100

    reproduction

---
+++ src/lib/context.svelte.ts
@@ -0,0 +1,27 @@
+export class Item {
+	prop: number;
+	constructor() {
+		this.prop = Math.random();
+	}
+
+	serialize() {
+		return JSON.stringify({ property: Math.random() });
+	}
+}
+
+export class Context {
+	items: Item[] = $state.raw([]);
+
+	addItem(item: Item) {
+		this.items.push(item);
+		this.#invalidate();
+	}
+
+	#invalidate() {
+		const memo = this.items;
+		this.items = null as unknown as Item[];
+		this.items = memo;
+	}
+}
+
+export const context = new Context();
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { context, Item } from '$lib/context.svelte';
+
+	async function addItem() {
+		const item = new Item();
+		context.addItem(item);
+	}
+</script>
+
+<svelte:boundary>
+	<button onclick={addItem}>Add</button>
+	{#each context.items as item (item.serialize())}
+		<div>{item.prop}</div>
+	{/each}
+</svelte:boundary>
